dc3642cf2d30c584f9582ac03457d5631ac589e8
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2024-04-30 23:55:29 +0800

    Update FuncfNOP.m

--- SNSCO/FuncfNOP.m
+++ SNSCO/FuncfNOP.m
@@ -3,8 +3,7 @@ function [objef, gradf, hessf] = FuncfNOP(x,lambda)
     objef = (lambda/2)*norm(x)^2-sum(x);
     if  nargout>1
         gradf = lambda*x-1;
-        hessf = lambda;  
+        hessf = lambda*eye(length(x));  
     end
 
 end
-
